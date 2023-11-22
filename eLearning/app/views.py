17ce12ea06e293f8608b9773c245c71cd88a0ae5
Bouslah Taha <bouslahtaha33@gmail.com>
2023-11-22 22:42:18 +1300

    CRUD operations for userModel

--- eLearning/app/views.py
+++ eLearning/app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    http_method_names = ["GET", "POST", "PUT", "PATCH", "DELETE"]
