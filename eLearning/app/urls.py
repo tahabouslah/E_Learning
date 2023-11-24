df2407eb9001bcad1e31d5bc223d7aade5c68752
Bouslah Taha <bouslahtaha33@gmail.com>
2023-11-25 07:49:06 +1300

    using the rest framework decorators for CRUD

--- eLearning/app/urls.py
+++ eLearning/app/urls.py
@@ -1,27 +1,7 @@
 from django.urls import path, include
-from rest_framework import routers
 from .views import *
 
-router = routers.DefaultRouter()
-router.register(r"users", UserViewSet, basename="users")
-router.register(
-    r"InteractionHistories", InteractionHistoryViewSet, basename="InteractionHistories"
-)
-router.register(
-    r"InteractionHistories", InteractionHistoryViewSet, basename="InteractionHistories"
-)
-router.register(
-    r"InteractionHistories", InteractionHistoryViewSet, basename="InteractionHistories"
-)
-router.register(
-    r"InteractionHistories", InteractionHistoryViewSet, basename="InteractionHistories"
-)
-router.register(
-    r"InteractionHistories", InteractionHistoryViewSet, basename="InteractionHistories"
-)
-router.register(
-    r"InteractionHistories", InteractionHistoryViewSet, basename="InteractionHistories"
-)
-
-
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("users/", user_list, name="user-list"),
+    path("users/<int:pk>/", user_detail, name="user-detail"),
+]
--- eLearning/app/views.py
+++ eLearning/app/views.py
@@ -11,28 +11,67 @@ from rest_framework.response import Response
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ["GET", "POST", "PUT"]
 
 
-class InteractionHistoryViewSet(viewsets.ModelViewSet):
-    queryset = InteractionHistory.objects.all()
-    serializer_class = InteractionHistorySerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
-class InteractionHistoryViewSet(viewsets.ModelViewSet):
-    queryset = InteractionHistory.objects.all()
-    serializer_class = InteractionHistorySerializer
+@api_view(["GET", "POST"])
+def user_list(request):
+    if request.method == "GET":
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class InteractionHistoryViewSet(viewsets.ModelViewSet):
-    queryset = InteractionHistory.objects.all()
-    serializer_class = InteractionHistorySerializer
+@api_view(["GET", "PUT", "DELETE"])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class InteractionHistoryViewSet(viewsets.ModelViewSet):
-    queryset = InteractionHistory.objects.all()
-    serializer_class = InteractionHistorySerializer
+# class InteractionHistoryViewSet(viewsets.ModelViewSet):
+#     queryset = InteractionHistory.objects.all()
+#     serializer_class = InteractionHistorySerializer
 
 
-class InteractionHistoryViewSet(viewsets.ModelViewSet):
-    queryset = InteractionHistory.objects.all()
-    serializer_class = InteractionHistorySerializer
+# class InteractionHistoryViewSet(viewsets.ModelViewSet):
+#     queryset = InteractionHistory.objects.all()
+#     serializer_class = InteractionHistorySerializer
+
+
+# class InteractionHistoryViewSet(viewsets.ModelViewSet):
+#     queryset = InteractionHistory.objects.all()
+#     serializer_class = InteractionHistorySerializer
+
+
+# class InteractionHistoryViewSet(viewsets.ModelViewSet):
+#     queryset = InteractionHistory.objects.all()
+#     serializer_class = InteractionHistorySerializer
