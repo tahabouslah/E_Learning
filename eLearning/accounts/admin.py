c57408130ad60284700a7c693e1c76ca592e1e6e
Bouslah Taha <bouslahtaha33@gmail.com>
2023-12-04 01:40:51 +1300

    changes

--- eLearning/accounts/admin.py
+++ eLearning/accounts/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-<<<<<<< HEAD:eLearning/app/admin.py
-
-admin.site.register(Assignment)
-=======
->>>>>>> 3bef0ff5b75ed0fb3e982d98d0d2e527004741b5:eLearning/accounts/admin.py
